efb8989ec1867cc7310500e9b42fc811a0aba42d
Joe DeVries <joe.devries@anchorops.com>
2016-09-29 10:05:15 -0400

    Upgrade Swagger-UI

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ module.exports = {
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.slideto.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.wiggle.min.js');
     app.import(app.bowerDirectory + '/underscore/underscore-min.js', { exports: { 'underscore-min': [ 'default' ] } });
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/handlebars-2.0.0.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/handlebars-4.0.5.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/backbone-min.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.7.3.pack.js', { exports: { 'hljs': ['default']}});
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.9.1.0.pack.js', { exports: { 'hljs': ['default']}});
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/swagger-oauth.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/swagger-ui.js');
 

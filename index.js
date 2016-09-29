45af4d1075026ae72b9f977bd06fcefed34515d6
Joe DeVries <joe.devries@anchorops.com>
2016-09-29 11:17:29 -0400

    Matching Javascript dependencies with example HTML file

--- index.js
+++ index.js
@@ -25,19 +25,19 @@ module.exports = {
     app.import(app.bowerDirectory + '/swagger-ui/dist/css/typography.css');
     app.import(app.bowerDirectory + '/swagger-ui/dist/css/screen.css');
 
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/marked.js');
-    app.import(app.bowerDirectory + '/jquery-migrate/jquery-migrate.min.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.ba-bbq.min.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/object-assign-pollyfill.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.slideto.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.wiggle.min.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/lodash.min.js');
-    app.import(app.bowerDirectory + '/underscore/underscore-min.js', { exports: { 'underscore-min': [ 'default' ] } });
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.ba-bbq.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/handlebars-4.0.5.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/lodash.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/backbone-min.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.9.1.0.pack.js', { exports: { 'hljs': ['default']}});
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jsoneditor.min.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/swagger-oauth.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/swagger-ui.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.9.1.0.pack.js', { exports: { 'hljs': ['default']}});
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.9.1.0.pack_extended.js', { exports: { 'hljs': ['default']}});
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jsoneditor.min.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/marked.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/swagger-oauth.js');
 
     app.import(app.bowerDirectory + '/swagger-ui/dist/o2c.html', { destDir: '/' });
   }

24a5cd8ed80563a1c3a5cb26130cfb153cd29848
Joe DeVries <joe.devries@anchorops.com>
2016-09-29 23:21:11 -0400

    Include JQuery Migrate

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ module.exports = {
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/object-assign-pollyfill.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.slideto.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.wiggle.min.js');
+    app.import(app.bowerDirectory + '/jquery-migrate/jquery-migrate.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/jquery.ba-bbq.min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/handlebars-4.0.5.js');
-    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/lodash.min.js');
+    app.import(app.bowerDirectory + '/swagger-ui/dist/lib/lodash.min.js', { exports: { 'lodash-compat': ['default']}});
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/backbone-min.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/swagger-ui.js');
     app.import(app.bowerDirectory + '/swagger-ui/dist/lib/highlight.9.1.0.pack.js', { exports: { 'hljs': ['default']}});
